3fbf265400c9960c5b9aecc69e2a050eab12a288
jjara1974 <jjara1974@gmail.com>
2019-12-12 19:29:29 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../modelos/user';
 import { LoginService } from './login.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   
-  user={
-    nombre:null,
-      };
-  usuarios: user[] = null;
+  usuarios: User[] = null;
 
   constructor(private loginservicio: LoginService) { }
 
   ngOnInit() {
     this.recuperarTodos();
-    console.log(this.usuarios[0].nombre);
+    console.log(this.usuarios);
   }
 
   recuperarTodos() {
-    this. loginservicio.recuperarTodos().subscribe(result => this.usuarios = result);
+   this.loginservicio.recuperarTodos()           //Llamamos a la funcion <strong>listar</strong> de nuestro servicio
+    .map((response) => response.json())   //Mapeamos los datos devueltos por nuestro archivo php
+    .subscribe((data) => {                
+      this.usuarios= data;   });
+    
+   
+
     
   }
+
 
 }
